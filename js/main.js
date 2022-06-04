afeddfe34aebb77c0aaa1c85e607a0b8ec3fca45
NadezhdaFilippova <n9685232364@yandex.ru>
2022-06-04 22:21:17 +0300

    corrections in common functions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Функция, возвращающая случ.число из диапазона включительно(из MDN)
 
-function getRandomFromRangeInclusive(min, max) {
+const getRandomFromRangeInclusive = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
 
@@ -8,17 +8,11 @@ function getRandomFromRangeInclusive(min, max) {
     throw new RangeError('Параметр должен быть больше или равным нулю');
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
+
 getRandomFromRangeInclusive();
 
 //проверка длины строки
 
-function checkStringLength(string, maxStringLength) {
-  if (string.length <= maxStringLength) {
-    return true;
-  }
-  return false;
-}
-
+const checkStringLength = (string, maxStringLength) => string.length <= maxStringLength;
 checkStringLength ();
-// Вопрос: в дискорде было обсуждение на счет проверки, является ли строка строкой. она нужна эта проверка? где эта функция может примениться еще, кроме подстчета символов в комментарии?
